79b9f3ec12c0c770fa6ca31240151f45cabe9ca9
Cataykus <cataykus@gmail.com>
2020-05-09 19:46:09 +0300

    q

---
+++ Unitydz03/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _runSpeed;
+    private Rigidbody2D playerRigidbody;
+    private Animator playerAnimator;
+    private SpriteRenderer playerSpriteRenderer;
+
+    private void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerRigidbody.MovePosition(playerRigidbody.position + Vector2.left * _runSpeed * Time.fixedDeltaTime);
+            playerSpriteRenderer.flipX = true;
+            playerAnimator.SetInteger("State", 1);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            playerRigidbody.MovePosition(playerRigidbody.position + Vector2.right * _runSpeed * Time.fixedDeltaTime);
+            playerSpriteRenderer.flipX = false;
+            playerAnimator.SetInteger("State", 1);
+        }
+        else
+        {
+            playerAnimator.SetInteger("State", 0);
+        }
+    }
+}
---
+++ Unitydz03/Assets/Scripts/Signaling.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Signaling : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out PlayerMovement player))
+        {
+            StartCoroutine(SignalingOn());
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out PlayerMovement player))
+        {
+            StartCoroutine(SignalingOff());
+        }
+    }
+
+    private IEnumerator SignalingOn()
+    {
+        _audioSource.Play();
+
+        for (int i = 0; i < 10; i++)
+        {
+            _audioSource.volume += 0.1f;
+
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
+    private IEnumerator SignalingOff()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            _audioSource.volume -= 0.1f;
+
+            yield return new WaitForSeconds(0.2f);
+        }
+
+        _audioSource.Stop();
+    }
+}
